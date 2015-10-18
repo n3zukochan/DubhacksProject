5c2560537e35607717085f3be63214d2c1c8cae8
Siddharth Rao <sidgrao@gmail.com>
2015-10-18 03:02:42 -0700

    working version

--- website/dubhackP.js
+++ website/dubhackP.js
@@ -2,52 +2,77 @@
 
 $(document).ready(function() {
 
+    var yes = false;
+    var no = false;
+
     if (annyang) {
         console.log("checking");
-      // Let's define our first command. First the text we expect, and then the function it should call
-      var commands = {
-        // 'show me :category': fetchCategory,
-        "show me :category": fetchCategory,
-        "play :article" : playArticle,
-        "next :article" : playTitle
-      };
+        // Let's define our first command. First the text we expect, and then the function it should call
+        var commands = {
+            // 'show me :category': fetchCategory,
+            "get :category": fetchCategory,
+            "play :article" : playArticle,
+            "next :article" : playTitle
+        };
 
-      annyang.addCommands(commands);
+        annyang.addCommands(commands);
 
-      annyang.start();
+        annyang.debug();
+        annyang.start();
 
+        //welcome
+        var audio = new Audio('http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/audio/welcome.mp3');
+        audio.play();
+        // sleep(700000);
     }
+
+
 
     //play instruction, play categories
    //wait for user to pick categories using annyang
    //if(we have that categories) {
    //fetchCategory("sports");
+
+
+    // fetchCategory("sports");
+
+
     var listOfAudios = [];
     var index = -1;
 
-   function fetchCategory(category) {
-       var URL = "http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category='sports'";
+    function fetchCategory(category) {
+       var URL = "http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category='" + category + "'";
        $.ajax({
            url: URL,
            type: 'get',
            complete: function(audio) {
+               console.log(audio.responseText)
                processAudio(audio.responseText);
            }
-       })
-
-   }
+       });
+    }
 
     function processAudio(audio) {
         var audio = JSON.parse(audio);
         for (var i = 0; i < audio.length; i++) {
             listOfAudios[i] = audio[i];
         }
-        playTitle();
-    });
 
-    function playTitle() {
+        // sleep(3000);
+        playTitle();
+    }
+
+    function sleep(miliseconds) {
+           var currentTime = new Date().getTime();
+
+           while (currentTime + miliseconds >= new Date().getTime()) {
+           }
+    }
+
+    function playTitle(cat) {
         //give instructiona
-        annyang.pause(); //?
+        console.log(index);
+        // annyang.pause(); //?
         index++;
         if(index < listOfAudios.length) {
             var titlename = listOfAudios[index].titlename;
@@ -64,5 +89,9 @@ $(document).ready(function() {
         audio.play();
         //if they say stop? what we do? --> start over again? or go back to titles.
         //prompt for another article, rate, never ask me to rate this agian?
-        playTitle();
+        // playTitle();
     }
+
+
+
+});
