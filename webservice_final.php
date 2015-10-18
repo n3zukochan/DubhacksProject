840a7f39fe598cca442d91e096e5951bbc3d8687
Siddharth Rao <sidgrao@gmail.com>
2015-10-17 21:03:05 -0700

    still sid doing stuff

--- webservice_final.php
+++ webservice_final.php
@@ -5,24 +5,7 @@
 
         The php webservice. Backend for dubhacks.
     */
-
-    error_reporting(E_ALL | E_STRICT);
-
-    // Defining server information.
-    // TODO: Put this information in a different file.
-    $server_name = "localhost";
-    $user_name = "root";
-    $password = "dubhacks";
-    $dbname = "dubhacks";
-
-    // Connecting to the database
-    $connection = new mysqli($server_name, $user_name, $password, $dbname);
-
-    // Check connection
-    if ($connection->connect_error) {
-        echo "connection failed";
-        // die("Connection Failed");
-    }
+    include("database_connect_credentials.php");
 
     if ($_SERVER["REQUEST_METHOD"] == 'GET') { // Clarissa and Cat
         if (isset($_GET["category"])) { // Checking if category is set.
