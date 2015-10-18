e66d7fcf3e217a94872574eea81f75822852ac17
CSONG09 <clarissasong@live.com>
2015-10-17 21:18:29 -0700

    js

--- website/dubhackP.js
+++ website/dubhackP.js
@@ -1,28 +1,38 @@
-
 //js linked to dubhackP.html
 
 $(document).ready(function() {
 
-    //play instruction, play categories
-    //wait for user to pick categories using annyang
-    //if(we have that categories) {
-        call fetchCategory(categories);
-    }
+   //play instruction, play categories
+   //wait for user to pick categories using annyang
+   //if(we have that categories) {
+    fetchCategory("sports");
+
+   //}
 });
 
-    function fetchCategory(category) {
-        var URL = "?=" + category;  
-        $.ajax({
-            url: URL,
-            type: 'GET',
-            complete: function(audio) {
-                processAudio(data.responseText);
-            }
-        })
+   function fetchCategory(category) {
+       var URL = "ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category=" + category;  
+       $.ajax({
+           url: URL,
+           type: 'GET',
+           complete: function(audio) {
+               processAudio(audio.responseText);
+           }
+       })
 
-    };
+   };
 
-    function processAudio(allAudios) {
+   function processAudio(audio) {
+       var audio = JSON.parse(audio);
+       
+       var text = $("test");
 
-    };
+       for (var i = 0; i < audio.length; i++) {
+           // test.append("<li>File name: " + audio[i].filename + " Tile name:" 
+           //     + audio[i].titlename + "</li>");
+            console.log(audio[i]);
+
+       }
+
+   };
 
