62fcd8f2b85428ca9fbd0c0ba7ae3b6721714bd9
CSONG09 <clarissasong@live.com>
2015-10-18 00:16:16 -0700

    updated js

--- website/dubhackP.js
+++ website/dubhackP.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
       // Let's define our first command. First the text we expect, and then the function it should call
       var commands = {
         // 'show me :category': fetchCategory,
-        "show me :dog": function() {
-            console.log("check!");
-        }
+        "show me :category": fetchCategory,
+        "play :article" : playArticle,
+        "next :article" : playTitle
       };
 
       annyang.addCommands(commands);
@@ -22,6 +22,8 @@ $(document).ready(function() {
    //wait for user to pick categories using annyang
    //if(we have that categories) {
    //fetchCategory("sports");
+    var listOfAudios = [];
+    var index = -1;
 
    function fetchCategory(category) {
        var URL = "http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category='sports'";
@@ -29,24 +31,38 @@ $(document).ready(function() {
            url: URL,
            type: 'get',
            complete: function(audio) {
-               //console.log(audio.responseText)
                processAudio(audio.responseText);
            }
        })
 
    }
 
-   function processAudio(audio) {
-       var audio = JSON.parse(audio);
-       console.log(audio.articles);
+    function processAudio(audio) {
+        var audio = JSON.parse(audio);
+        for (var i = 0; i < audio.length; i++) {
+            listOfAudios[i] = audio[i];
+        }
+        playTitle();
+    });
 
-    //    var text = $("test");
-    //    for (var i = 0; i < audio.length; i++) {
-    //        // test.append("<li>File name: " + audio[i].filename + " Tile name:"
-    //        //     + audio[i].titlename + "</li>");
-    //         console.log(audio[i]);
-       //
-    //    }
+    function playTitle() {
+        //give instructiona
+        annyang.pause(); //?
+        index++;
+        if(index < listOfAudios.length) {
+            var titlename = listOfAudios[index].titlename;
+            var audio = new Audio('http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/audio/' + titlename);
+            audio.play();
+            //Prompt user to make a choice 
+            annyang.start();
+        }
+    }
 
-   }
-});
+    function playArticle() {
+        var articlename = listOfAudios[index].articlename;
+        var audio = new Audio('http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/audio/' + articlename);
+        audio.play();
+        //if they say stop? what we do? --> start over again? or go back to titles. 
+        //prompt for another article, rate, never ask me to rate this agian?
+        playTitle();
+    }
