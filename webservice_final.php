f3443b7c585cd03fe958573566dc0440c7826615
Siddharth Rao <sidgrao@gmail.com>
2015-10-17 20:46:36 -0700

    Added the webservice. -s

--- webservice_final.php
+++ webservice_final.php
@@ -1,12 +1,12 @@
 <?php
     /*
         Name: Sid Rao
+        Team members: Clarissa, Catherine, Daniel, Hiep
 
         The php webservice. Backend for dubhacks.
     */
 
     error_reporting(E_ALL | E_STRICT);
-
 
     // Defining server information.
     // TODO: Put this information in a different file.
@@ -20,15 +20,27 @@
 
     // Check connection
     if ($connection->connect_error) {
-        die("Connection Failed");
+        echo "connection failed";
+        // die("Connection Failed");
     }
 
     if ($_SERVER["REQUEST_METHOD"] == 'GET') { // Clarissa and Cat
         if (isset($_GET["category"])) { // Checking if category is set.
+
             $category = $_GET["category"]; // The category desired
+            $final_result = array();
+            $get_file_names_query = "select filename, titlename from audiotable
+                     where category = $category";
 
-            $query =
+            $result = $connection->query($get_file_names_query);
+
+            while ($row = $result->fetch_assoc()) {
+               array_push($final_result, $row);
+            }
+
+            echo json_encode($final_result);
         }
+    } else {
+        echo "method was not get.";
     }
-
 ?>
