fe626e653d1335be9d106583511499e25a004aa9
CSONG09 <clarissasong@live.com>
2015-10-17 21:54:59 -0700

    s

--- website/dubhackP.js
+++ website/dubhackP.js
@@ -5,23 +5,41 @@ $(document).ready(function() {
 
    //play instruction, play categories
    //wait for user to pick categories using annyang
+    
+
+
+    if (annyang) {
+      // Let's define our first command. First the text we expect, and then the function it should call
+
+
+      var commands = {
+        'show me :category': fetchCategory,
+      };
+
+      annyang.addCommands(commands);
+
+      annyang.start();
+      
+    }
    //if(we have that categories) {
-    fetchCategory("sports");
+ //   fetchCategory("sports");
 
    //}
 });
 
    function fetchCategory(category) {
-       var URL = "ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category=" + category;  
-       $.ajax({
-           url: URL,
-           type: 'GET',
-           complete: function(audio) {
-               processAudio(audio.responseText);
-           }
-       })
 
-   };
+        console.log("worked");
+       // var URL = "http://ec2-52-89-34-199.us-west-2.compute.amazonaws.com/webservice_final.php?category=" + category;  
+       // $.ajax({
+       //     url: URL,
+       //     type: 'GET',
+       //     complete: function(audio) {
+       //         processAudio(audio.responseText);
+       //     }
+       // })
+
+   }
 
    function processAudio(audio) {
        var audio = JSON.parse(audio);
@@ -32,8 +50,9 @@ $(document).ready(function() {
            // test.append("<li>File name: " + audio[i].filename + " Tile name:" 
            //     + audio[i].titlename + "</li>");
             console.log(audio[i]);
+            console.log("worked");
 
        }
 
-   };
+   }
 
